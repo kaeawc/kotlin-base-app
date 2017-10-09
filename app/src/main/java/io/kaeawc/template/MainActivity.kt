e86e1b703ea01bb6fc6500b4cc5624679c798ff9
Jason Pearson <jason.d.pearson@gmail.com>
2017-10-08 23:36:57 -0400

    remove dagger and glide

--- app/src/main/java/io/kaeawc/template/MainActivity.kt
+++ app/src/main/java/io/kaeawc/template/MainActivity.kt
@@ -2,7 +2,6 @@ package io.kaeawc.template
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import io.reactivex.disposables.CompositeDisposable
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -13,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppComponent.init(application as App).inject(this)
     }
 
     override fun onResume() {
